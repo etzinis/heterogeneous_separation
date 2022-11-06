52eb1fc03b3516bbcf1602e777c82b89a865fe31
etzinis <etzinis@gmail.com>
2022-11-06 00:23:23 -0500

    Add rest of the paths for voxforge and wsj

--- __config__.py
+++ __config__.py
@@ -6,7 +6,20 @@ import os
 """Fill in the path with the absolute path of the repo."""
 ROOT_DIRPATH = "/home/thymios/MERL/code/github_projects/heterogeneous_separation/"
 
+"""Datasets paths"""
+WHAM_ROOT_PATH = "/db/original/public/WHAM/wham/"
+WHAMR_ROOT_PATH = "/db/original/public/WHAM/whamr/"
 
+"""Metadata text files for datasets"""
+WHAM_METADATA_P = os.path.join(
+    ROOT_DIRPATH, "heterogeneous_separation/dataset_loader/wham_speaker_gender_info.txt")
+WHSJ02MIX_METADATA_P = os.path.join(
+    ROOT_DIRPATH, "heterogeneous_separation/dataset_loader/wsj02mix_metadata.txt")
+
+# Path fo
+VOXFORGE_DATA_P = "/mnt/data/Speech//voxforge_alllang"
+VOXFORGE_METADATA_P = os.path.join(
+    ROOT_DIRPATH, "heterogeneous_separation/dataset_loader/voxforge_metadata.pkl")
 
 # Point to the directory where all librispeech partitions lie in:
 # e.g. train-clean-360, test-clean, etc.
